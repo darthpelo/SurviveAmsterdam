55be1120d378c939c89d0b07a2176c68434dd3e0
Alessio Roberto <aroberto@mobiquityinc.com>
2016-03-05 19:13:53 +0100

    Base UI and location strings

--- SurviveAmsterdam/Location/ViewControllers/LocationsListViewController.swift
+++ SurviveAmsterdam/Location/ViewControllers/LocationsListViewController.swift
@@ -9,21 +9,28 @@
 import UIKit
 
 final class LocationCell: UITableViewCell {
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var shopNameLabel: UILabel!
+    @IBOutlet weak var shopAddressLabel: UILabel!
     
 }
 
 final class LocationsListViewController: UIViewController {
 
-
+    override func viewDidLoad() {
+        title = NSLocalizedString("shops", comment: "")
+    }
 }
 
 extension LocationsListViewController: UITableViewDataSource {
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
+        return 0
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(R.reuseIdentifier.locationCell.identifier, forIndexPath: indexPath) as! LocationCell
         
+        return cell
     }
     
 }
--- SurviveAmsterdam/Product/ViewControllers/ProductsListViewController.swift
+++ SurviveAmsterdam/Product/ViewControllers/ProductsListViewController.swift
@@ -8,8 +8,33 @@
 
 import UIKit
 
-class ProductsListViewController: UIViewController {
+final class ProductCell: UITableViewCell {
+    
+    @IBOutlet weak var thumbView: UIImageView!
+    @IBOutlet weak var productNameLabel: UILabel!
+    @IBOutlet weak var shopNameLabel: UILabel!
+}
 
+final class ProductsListViewController: UIViewController {
 
+    override func viewDidLoad() {
+        title = NSLocalizedString("products", comment: "")
+    }
 
+}
+
+extension ProductsListViewController: UITableViewDataSource {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(R.reuseIdentifier.productCell.identifier, forIndexPath: indexPath) as! ProductCell
+        
+        return cell
+    }
+}
+
+extension ProductsListViewController: UITableViewDelegate {
+    
 }
