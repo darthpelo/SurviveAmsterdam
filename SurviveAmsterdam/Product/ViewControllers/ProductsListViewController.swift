f108915cb61ca2e179029b0938f6f47b6a823ee3
Alessio Roberto <aroberto@mobiquityinc.com>
2016-04-18 22:53:36 +0200

    Implement product search in the products list viewcontroller

--- SurviveAmsterdam/Product/ViewControllers/ProductsListViewController.swift
+++ SurviveAmsterdam/Product/ViewControllers/ProductsListViewController.swift
@@ -22,8 +22,25 @@ final class ProductsListViewController: UIViewController {
     private var productsList: Results<(Product)>?
     private let modelManager = ModelManager()
     
+    private var searchController:UISearchController = UISearchController(searchResultsController: nil)
+    private var productsSearchResults:Results<(Product)>?
+    
     override func viewDidLoad() {
         title = NSLocalizedString("products", comment: "")
+        
+        searchController.searchResultsUpdater = self
+        searchController.dimsBackgroundDuringPresentation = false
+        if #available(iOS 9.0, *) {
+            searchController.loadViewIfNeeded()
+        } else {
+            searchController.searchBar.sizeToFit()
+        }
+        searchController.searchBar.placeholder = NSLocalizedString("searchbar.placeolder", comment: "")
+        searchController.searchBar.delegate = self
+        searchController.searchBar.tintColor = UIColor.whiteColor()
+        definesPresentationContext = true
+        
+        tableView.tableHeaderView = searchController.searchBar
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -61,7 +78,7 @@ extension ProductsListViewController: UITableViewDataSource {
             // handle delete (by removing the data from your array and updating the tableview)
             if let product = productsList?[indexPath.row] {
                 do {
-                    try ModelManager().deleteProcut(product)
+                    try modelManager.deleteProcut(product)
                     tableView.reloadData()
                 } catch ModelManagerError.DeleteFailed {
                     print(ErrorType)
@@ -98,4 +115,39 @@ extension ProductsListViewController: UITableViewDelegate {
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         performSegueWithIdentifier(R.segue.productsListViewController.productDetailSegue, sender: self)
     }
+}
+
+extension ProductsListViewController: UISearchBarDelegate, UISearchResultsUpdating {
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        do {
+            productsList = try modelManager.getProducts()
+        } catch {
+            //
+        }
+        
+        tableView.reloadData()
+    }
+    
+    func updateSearchResultsForSearchController(searchController: UISearchController) {
+        if let query = searchController.searchBar.text where searchController.active && !query.isEmpty {
+            do {
+                try productsList = modelManager.getProductsMatching(query)
+                tableView.reloadData()
+            } catch {}
+        }
+    }
+}
+
+extension UISearchBar {
+    
+    func setBarTintColorWithAnimation(color:UIColor) {
+        let transition = CATransition()
+        transition.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+        transition.type = kCATransitionFade
+        transition.duration = 0.2
+        
+        layer.addAnimation(transition, forKey: nil)
+        barTintColor = color
+    }
+    
 }
